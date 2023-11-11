8c9705d3c9764ae27961b49d21176a9f281a9e44
hmyo2853 <hmyo2853@gmail.com>
2023-11-11 22:58:56 +0900

    [코드 수정] site.config.js 오류 수정

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   // CONFIG configration (required)
   link: "https://gelee-devlog.vercel.app/",
   since: 2023, // If leave this empty, current year will be used.
-  lang: ["en-US", "ko-KR"], // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app",
 
   // notion configuration (required)
